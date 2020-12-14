a49c91abb962e7a71f8c43ccb04dfee3d0fe531d
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-14 17:53:26 -0300

    userForm imported to app js

--- src/App.js
+++ src/App.js
@@ -1,47 +1,53 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { incrementar, decrementar, setear } from './reducers'
+import UserForm from './components/UserForm'
 import logo from './logo.svg'
 import './App.css'
 
 
 class App extends Component {
-  handleSetear = e => {
-    const { setear } = this.props
-    const { valor } = this.state 
-    setear(Number(valor)) 
-  }
+  // handleSetear = e => {
+  //   const { setear } = this.props
+  //   const { valor } = this.state 
+  //   setear(Number(valor)) 
+  // }
 
-  handleChange = e => {
-     const { name, value } = e.target
-     this.setState({ [name]: value })
+  // handleChange = e => {
+  //    const { name, value } = e.target
+  //    this.setState({ [name]: value })
+  // }
+
+  handleSubmit = payload => {
+    console.log(payload)
   }
 
   render() {
-    const { incrementar, decrementar, valor  } = this.props
+    // const { incrementar, decrementar, valor  } = this.props
     return (
       <div className="App">
-        <p>{valor}</p>
+
+        <UserForm onSubmit={this.handleSubmit} />
+        {/* <p>{valor}</p>
         <button onClick={incrementar} >Incrementar</button>
         <button onClick={decrementar} >Decrementar</button>
         <input name='valor' onChange={this.handleChange} />
-        <button onClick={this.handleSetear} >Setear</button>
+        <button onClick={this.handleSetear} >Setear</button> */}
       </div>
     );
   }
 }
 
 const mapStateToProps = state => {
-  console.log(state)
-  return {
+   return {
     valor: state.contador,
   }
 }  
 
 const mapDispatchToProps = dispatch => ({
-  incrementar: () => dispatch(incrementar()),
-  decrementar: () => dispatch(decrementar()),
-  setear: payload => dispatch(setear(payload)),
+  // incrementar: () => dispatch(incrementar()),
+  // decrementar: () => dispatch(decrementar()),
+  // setear: payload => dispatch(setear(payload)),
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
-import { reduxForm } from 'redux-form'
+import { reduxForm, Field  } from 'redux-form'
 
 class UserForm extends Component {
     render() {
         const { handleSubmit } = this.props
         return(
             <form onSubmit={handleSubmit} >
-
+                <Field name="name" component="input" placeholder="name" />
+                <Field name="lastname" component="input" placeholder="last name" />
+                <input type="submit" value="Enviar"/>
             </form>
         )
     }
