0eda347fbe9165465278811faad814109cc7ee46
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-13 23:09:10 -0300

    provider was implemented at render

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ import App from './App'
 import reportWebVitals from './reportWebVitals'
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  </Provider>
+  , document.getElementById('root'));
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
